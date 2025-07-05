541acd0526c040c02155c1770174e68860c55769
InvestUz <invest.abdusattorov@gmail.com>
2025-07-05 17:03:07 +0500

    Приложения has been added

--- resources/views/pages/frontend/investment-projects.blade.php
+++ resources/views/pages/frontend/investment-projects.blade.php
@@ -289,7 +289,7 @@
                             </span>
                             <span class="btn-text">Объявление</span>
                         </a>
-                        <a href="приложение.zip" class="action-btn secondary" download>
+                        <a href="{{asset('assets/folders/Приложения.zip')}}" class="action-btn secondary" download>
                             <span class="btn-icon">
                                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M7,10L12,15L17,10H7Z" fill="currentColor"/>
